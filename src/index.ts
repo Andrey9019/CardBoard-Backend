e69f2f8323b0ba1a9f0e2c1d13416a266fcc78c7
Andrey9019 <1234andrey5678@gmail.com>
2025-07-22 18:25:39 +0300

    Add port listening for Render

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import swaggerConfig from "../swagger";
+import swaggerConfig from "../swagger.js";
 import productsRouter from "./routes/products";
 import productIdRouter from "./routes/product-id";
 import categoriesRouter from "./routes/categories";
@@ -15,18 +15,16 @@ app.use(cors());
 swaggerConfig(app);
 
 app.use("/api/products", productsRouter);
-app.use("/api/products/", productIdRouter);
+app.get("/api/products/", productIdRouter);
 app.use("/api/all_categories", categoriesRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Products API!");
 });
 
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 3001;
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-}
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 export default app;
