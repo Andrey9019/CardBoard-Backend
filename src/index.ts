81d7d7bf0b624d2f060a3772ba82342b161fadf2
Andrey9019 <1234andrey5678@gmail.com>
2025-07-22 14:23:24 +0300

    Configure Vercel routes and CORS

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import cors from "cors";
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import swaggerConfig from "../swagger";
 import productsRouter from "./routes/products";
@@ -9,13 +9,12 @@ import categoriesRouter from "./routes/categories";
 dotenv.config();
 const app = express();
 
+app.use(express.json());
 app.use(
   cors({ origin: ["https://card-board.vercel.app", "http://localhost:3000"] })
 );
 
 swaggerConfig(app);
-
-app.use(express.json());
 
 app.use("/api/products", productsRouter);
 app.use("/api/products/", productIdRouter);
