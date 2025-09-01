720306c7037a337f2d86a6b75fd9a90731a5a203
Andrey9019 <1234andrey5678@gmail.com>
2025-09-01 15:42:47 +0300

    Add prisma generate to build script

--- src/lib/prisma-client.ts
+++ src/lib/prisma-client.ts
@@ -1,14 +1,24 @@
-/* eslint-disable no-var */
+// /* eslint-disable no-var */
+// import { PrismaClient } from "@prisma/client";
+
+// const prismaClientSingleton = () => {
+//   return new PrismaClient();
+// };
+
+// declare global {
+//   var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+// }
+
+// export const prisma = global.prismaGlobal ?? prismaClientSingleton();
+
+// if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+
 import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
-
-export const prisma = global.prismaGlobal ?? prismaClientSingleton();
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
