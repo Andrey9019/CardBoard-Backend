ea613dcb212085c6f65b05a3cea47964046092c9
Andrey9019 <1234andrey5678@gmail.com>
2025-10-18 16:11:07 +0300

    update redmi / swagger

--- src/app/api/docs/route.ts
+++ src/app/api/docs/route.ts
@@ -1,26 +1,3 @@
-// import { NextResponse } from 'next/server';
-// import swaggerJsdoc from 'swagger-jsdoc';
-
-// const options = {
-//   definition: {
-//     openapi: '3.0.0',
-//     info: {
-//       title: 'Card&Board API',
-//       version: '1.0.0',
-//       description: 'API для інтернет-магазину настільних ігор Card&Board',
-//     },
-//     servers: [{ url: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3001' }],
-//   },
-//   apis: ['./src/app/api/**/route.ts'],
-// };
-
-// const specs = swaggerJsdoc(options);
-
-// export async function GET() {
-//   return  NextResponse.json(specs)
-// }
-
-
 import { NextResponse } from 'next/server';
 
 export async function GET(req: Request) {
